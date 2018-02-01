c93b4a1d17ce1c24359e90f55109494e8e1e204d
CHUNG TRAN <chungtran@CHUNGs-iMac.local>
2018-01-31 22:50:23 -0800

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by CHUNG TRAN on 1/31/18.
+//  Copyright © 2018 CHUNG TRAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
